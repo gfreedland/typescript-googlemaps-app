1e9c523b8b3b1dac010eef369a1afe563f9ba3a4
gfreedland <gfreedland@mail.sfsu.edu>
2020-05-14 22:07:44 -0700

    finished app

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,29 @@
 "use strict";
-console.log('hi');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const form = document.querySelector('form');
+const addressInput = document.getElementById('address');
+const GOOGLE_API_KEY = 'AIzaSyB8gwpYo2tO2isLPlSxOp-_aaNgxwrQsYw';
+function onSearch(event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    axios_1.default.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`).then(response => {
+        if (response.data.status !== 'OK') {
+            throw new Error('Could not fetch location');
+        }
+        const coordinates = response.data.results[0].geometry.location;
+        const map = new google.maps.Map(document.getElementById("map"), {
+            center: coordinates,
+            zoom: 16
+        });
+        new google.maps.Marker({ position: coordinates, map: map });
+    }).catch(err => {
+        alert(err.message);
+        console.log(err);
+    });
+}
+form.addEventListener('submit', onSearch);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,43 @@
-console.log('hi');
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyB8gwpYo2tO2isLPlSxOp-_aaNgxwrQsYw';
+
+// declare var google: any;
+
+type GoogleGeocodingResponse = {
+    results: {geometry: {location: {lat: number, lng: number}}}[];
+    status: 'OK' | 'ZERO_RESULTS';
+};
+
+function onSearch(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+    // Send to Google's API
+    // fetch('');
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+            enteredAddress
+        )}&key=${GOOGLE_API_KEY}`
+    ).then(response => {
+        // console.log(response);
+        if (response.data.status !== 'OK') {
+            throw new Error('Could not fetch location');
+        }
+        // Sets Coordinates
+        const coordinates = response.data.results[0].geometry.location; 
+        // Renders Map  
+        const map = new google.maps.Map(document.getElementById("map")!, {
+            center: coordinates,
+            zoom: 16
+        });
+        // Renders Marker
+        new google.maps.Marker({position: coordinates, map: map});
+    }).catch(err => {
+        alert(err.message);
+        console.log(err);
+    });
+}
+
+form.addEventListener('submit', onSearch);
